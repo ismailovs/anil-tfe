d11063079e302c07ef0cad51cbe7284bfeefe942
Shukhrat Ismailov <shuhx7@gmail.com>
2023-10-04 17:13:07 -0400

    changes

--- main.tf
+++ main.tf
@@ -1,21 +1,47 @@
 terraform {
-  required_providers {
-    tfe = {
-      version = "~> 0.49.1"
-    }
-  }
-
-
-  cloud {
+  
+   cloud {
     organization = "s_tc_1"
 
     workspaces {
       name = "tfe-workspace-exercise"
       }
     }
+
+
+   required_providers {
+     tfe = {
+     version = "~> 0.49.1"
+           }
+  
+      }
 }
 
+
 provider "tfe" {
-  version = "~> 0.49.1"
-  ...
+      version = "~>0.49.1"  
+}
+
+
+
+data "tfe_organization" "s" {
+  name = "s_tc_1"
+}
+
+
+locals {
+  exec_type = "local"
+  customers = [
+    "apple",
+    "google",
+    "twitter",
+  ]
+}
+
+
+resource "tfe_workspace" "test" {
+  for_each = toset(local.customers)
+  name           = each.key
+  organization   = data.tfe_organization.s.name
+  execution_mode = local.exec_type
 }
